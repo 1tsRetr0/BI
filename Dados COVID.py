242d414ab0c0be5fb9366eddbd5486e7573fec6f
1tsRetr0 <94655702+1tsRetr0@users.noreply.github.com>
2023-03-21 02:44:27 -0300

    Add files via upload

---
+++ Dados COVID.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+tabela1 = pd.read_csv(
+    'Dados Covid/MICRODADOS.csv',
+    sep=';',
+    encoding='latin_1'
+)
+
+print(
+    tabela1.query(
+        'Municipio == "CARIACICA" and ComorbidadeTabagismo == "Sim" and "2020-01-01" <= DataObito <= "2023-12-31"'
+    )
+)
